d74e17931e635a680e6e424de2d7f18aff2e483d
Ali <alym11731996@gmail.com>
2022-02-12 23:40:18 +0300

    toplama methodu added

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -7,7 +7,8 @@ namespace Methods
         static void Main(string[] args)
         {
             //yaz();
-            yazz();
+            //yazz();
+            topla();
             Console.ReadLine();
         }
 
@@ -17,16 +18,27 @@ namespace Methods
         //    Console.ReadLine();
         //}
 
-        static void yazz() //“Merhaba” metnini bir terimle hafızaya alıp, kullanıcının istediği adette hafızaya alınan terimle yazdırma yöntemi.
+        //static void yazz() //“Merhaba” metnini bir terimle hafızaya alıp, kullanıcının istediği adette hafızaya alınan terimle yazdırma yöntemi.
+        //{
+        //    Console.WriteLine("Mesajınızı giriniz");
+        //    string mesaj = Console.ReadLine();
+        //    Console.WriteLine("Ekranda mesajınız kaç kere görülsün?");
+        //    int adet = Convert.ToInt32(Console.ReadLine());
+        //    for (int i = 0; i < adet; i++)
+        //    {
+        //        Console.WriteLine(mesaj);
+        //    }
+        //}
+
+        static void topla()
         {
-            Console.WriteLine("Mesajınızı giriniz");
-            string mesaj = Console.ReadLine();
-            Console.WriteLine("Ekranda mesajınız kaç kere görülsün?");
-            int adet = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < adet; i++)
-            {
-                Console.WriteLine(mesaj);
-            }
+            int toplam = 0;
+            Console.WriteLine("Toplama yapmak istediğiniz 1. sayıyı giriniz:");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Toplama yapmak istediğiniz 2. sayıyı giriniz:");
+            int sayi2 = int.Parse(Console.ReadLine());
+            toplam = sayi1 + sayi2;
+            Console.WriteLine("Toplam="+toplam);
         }
     }
 }
